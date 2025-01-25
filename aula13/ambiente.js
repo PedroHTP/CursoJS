47f2ed7b4420282574e8e32f40d6a411287320c2
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-01-24 22:06:57 -0300

    debug e arrays

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+/*
+    var c = 1
+    do {
+        console.log(`${c}`)
+        c++
+    } while (c <= 500)
+*/
+
+/*
+    while (c <= 500) {
+        console.log(`${c}`)
+        c++
+    } 
+*/
+
+    var beta = ['Omega', 'Alpha', 'Gama', 'Falcon', 'Delta', 'Charlie']
+
+    for (let C = 0; C < beta.length; C++) {
+        const x = beta[C];
+        console.log(x)
+    }
